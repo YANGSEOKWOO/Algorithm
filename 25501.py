4612711a7537282e92e737c23cc78f3685e15ecb
YANGSEOKWOO <didtjrdn030@naver.com>
2023-02-18 00:49:43 +0900

    재귀

---
+++ 25501.py
@@ -0,0 +1,16 @@
+def recursion(text,st, ed, cnt):
+    if (st >= ed): return 1, cnt
+    else:
+        if text[st] != text[ed]: return 0, cnt
+        else:
+            cnt +=1
+            return recursion(text, st+1, ed-1, cnt)
+
+n = int(input())
+for i in range(n):
+    word = input()
+    cnt = 1
+    a, b = recursion(word, 0, len(word)-1, cnt)
+    print(a, b)
+    
+    
