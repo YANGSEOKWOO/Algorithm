6f5e8e39016d3f23601037ec8492bfab9bbcb7fa
YANGSEOKWOO <59641097+YANGSEOKWOO@users.noreply.github.com>
2025-05-13 00:18:17 +0900

    [level 2] Title: 완전범죄, Time: 0.12 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -0,0 +1,20 @@
+# 1. a: 내림차순으로 정렬
+# 2. a가 같다면, b는 오름차순
+def solution(info, n, m):
+    sort_info = sorted(info, key= lambda x: (-(x[0]-x[1]), -x[0], x[1]))
+    print(sort_info)
+    
+    a_cnt = n
+    b_cnt = m
+    a_recur = 0
+    for a_info, b_info in sort_info:
+        print(f'a_info:{a_info}, b_info:{b_info} a_cnt:{a_cnt} b_cnt:{b_cnt} a_recur: {a_recur}', )
+        if b_cnt > b_info:
+            b_cnt -= b_info
+        else:
+            if a_cnt > a_info:
+                a_cnt -= a_info
+                a_recur += a_info
+            else:
+                return -1
+    return a_recur
